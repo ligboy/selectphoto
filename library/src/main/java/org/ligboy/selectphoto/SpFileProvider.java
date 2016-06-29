ef5093ca2ebcb6404c2eb8d275a88a0e8d71cd4c
ligboy <ligboy.liu@heysroad.com>
2016-06-29 23:36:52 +0800

    Fix the android platform bug.

---
+++ library/src/main/java/org/ligboy/selectphoto/SpFileProvider.java
@@ -0,0 +1,10 @@
+package org.ligboy.selectphoto;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * @author Ligboy.Liu ligboy@gmail.com.
+ */
+//see http://code.google.com/p/android/issues/detail?id=7716
+public class SpFileProvider extends FileProvider {
+}
